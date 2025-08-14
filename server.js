ddb6da581ef0a8c8c398c2d40c9eaa1f19179a15
unknown <ron.garcia.santos@gmail.com>
2025-08-14 23:23:53 +0800

    MVC Run Test

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
-
 const app = require('./src/app');
+
+// Middleware
 app.use(cors());
 
+// Port
 const PORT = process.env.PORT || 3000;
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
