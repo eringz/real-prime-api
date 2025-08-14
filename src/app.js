5dd0608e62bab145e13860b5b7e98b8cdde1bdda
unknown <ron.garcia.santos@gmail.com>
2025-08-14 23:14:20 +0800

    MVC Run Test

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoutes');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+/**
+ * ------------------------------------- ROUTES SECTION -------------------------------------
+ */
+app.use('/api/auth', authRoutes);
+
+
+/**
+ *  ------------------------------------- URL SECTION -------------------------------------
+ */
+app.get('/', (req, res) => {
+    res.send('Real Prime is running!');
+});
+
+module.exports = app;
