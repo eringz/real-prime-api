e4989edf1870a26d4db17dad3de13768a6a15ba9
unknown <ron.garcia.santos@gmail.com>
2025-08-14 22:19:42 +0800

    Run Test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: 'Real Prime API - Staging Environment'});
+    res.json({message: 'Real Prime API - Staging Environment sa inyong lahat. hahaha'});
 });
 
 const PORT = process.env.PORT || 3000;
