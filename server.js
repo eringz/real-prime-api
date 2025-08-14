960cdd8bb0548ec755e1508d6b87f9f70011c92b
unknown <ron.garcia.santos@gmail.com>
2025-08-14 23:14:49 +0800

    MVC Run test

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
 
-const app = express();
+const app = require('./src/app');
 app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.json({message: 'Real Prime API - Staging Environment sa inyong lahat. hahaha'});
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
