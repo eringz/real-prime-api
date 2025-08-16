cb9c7bb2b2b4512d1c5a74028bb2fee943e80fc2
unknown <ron.garcia.santos@gmail.com>
2025-08-17 00:30:25 +0800

    User Test

--- src/app.js
+++ src/app.js
@@ -1,31 +1,11 @@
-// app.js
 import express from "express";
-import authRoutes from "./routes/userRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+
 app.use(express.json());
 
 // Routes
-app.use("/auth", authRoutes);
-
-// 405 handler (Method Not Allowed)
-app.use((req, res, next) => {
-  if (req.method !== "POST" && req.originalUrl.startsWith("/auth")) {
-    return res.status(405).json({
-      title: "405 Method Not Allowed",
-      message: "The method is not allowed for the requested URL."
-    });
-  }
-  next();
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    title: "500 Internal Server Error",
-    message: err.message || "Something went wrong!"
-  });
-});
+app.use("/api/users", userRoutes);
 
 export default app;
