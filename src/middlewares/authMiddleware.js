28ee3f39db6f70fb862fe16373594c557d19bca9
unknown <ron.garcia.santos@gmail.com>
2025-08-20 15:08:10 +0800

    Authentication Test

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import {ENV} from '../config/env.js';
 
 export const authMiddleware = (req, res, next) => {
-    console.log(`headers: ${req.heasers.authorization}`);
+    console.log(`headers: ${req.headers.authorization}`);
     const hdr = req.headers.authorization || '';
     const parts = hdr.split(' ');
     if (parts.length !== 2 || parts[0] !== 'Bearer') return res.status(401).json({message: 'Missing or invalid Authorization header'}); 
